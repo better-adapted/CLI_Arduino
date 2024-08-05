0dda62b733fd03b75db992b60060ebb0b068bf0b
Adam Watson <adam_t_watson@hotmail.com>
2024-08-05 18:49:30 +0100

    mod to quiet down warnings in eclipse esp-idf

--- src/CLI.cpp
+++ src/CLI.cpp
@@ -1,5 +1,7 @@
 #include "CLI.h"
 
+#include <Arduino.h>
+
 int32_t CLI_Scan_INT32_Setting(Stream *Port, String pInput, String pCMD, int32_t *pValue, int32_t pMin, int32_t pMax, uint32_t pBase,String pOutputPrompt)
 {	
 	int ret_val = 0;
