80ca252664f6377a87669e05b58192fd27875795
Adam Watson <adam_t_watson@hotmail.com>
2024-08-19 16:55:26 +0100

    aded float and int16 support

--- src/CLI.cpp
+++ src/CLI.cpp
@@ -2,6 +2,73 @@
 
 #include <Arduino.h>
 
+float CLI_Scan_float_Setting(Stream *Port, String pInput, String pCMD, float *pValue, float pMin, float pMax,const char* pFormat,String pOutputPrompt)
+{
+	int ret_val = 0;
+
+	if (pInput.startsWith(pCMD))
+	{
+		String ResponseAppendString = "";
+
+		int index = pCMD.length();
+
+		bool echo_value = false;
+
+		if (pInput[index] == '?')
+		{
+			ret_val |= CLI_SCAN_GET_OK;
+			echo_value = true;
+		}
+		else if (pInput[index] == '=')
+		{
+			int temp_value = strtof(pInput.substring(index + 1).c_str(), 0);        // decimal conversion
+
+			echo_value = true;
+
+			// check the value is within range
+			if(temp_value > pMax)
+			{
+				ret_val |= CLI_SCAN_SET_ERR_MAX;       // out of range!
+				ResponseAppendString += ",ERR_MAX";
+			}
+			else if(temp_value < pMin)
+			{
+				ret_val |= CLI_SCAN_SET_ERR_MIN;       // out of range!
+				ResponseAppendString += ",ERR_MIN";
+			}
+			else
+			{
+				if (*pValue != temp_value)
+				{
+					*pValue = temp_value;
+					ret_val |= CLI_SCAN_SET_OK;
+					ResponseAppendString += ",OK";
+				}
+				else
+				{
+					ResponseAppendString += ",SAME";
+				}
+			}
+		}
+		else
+		{
+			// invalid input - not ? or =
+			return ret_val;
+		}
+
+		if (echo_value)
+		{
+			Port->print(pOutputPrompt);
+			Port->print(pCMD);
+			Port->print("=");
+			Port->printf(pFormat,*pValue);
+			Port->println(ResponseAppendString);
+		}
+	}
+
+	return ret_val;
+}
+
 int32_t CLI_Scan_INT32_Setting(Stream *Port, String pInput, String pCMD, int32_t *pValue, int32_t pMin, int32_t pMax, uint32_t pBase,String pOutputPrompt)
 {	
 	int ret_val = 0;
@@ -66,6 +133,73 @@ int32_t CLI_Scan_INT32_Setting(Stream *Port, String pInput, String pCMD, int32_t
 		}		
 	}
 		
+	return ret_val;
+}
+
+int16_t CLI_Scan_INT16_Setting(Stream *Port, String pInput, String pCMD, int16_t *pValue, int16_t pMin, int16_t pMax, uint32_t pBase,String pOutputPrompt)
+{
+	int ret_val = 0;
+
+	if (pInput.startsWith(pCMD))
+	{
+		String ResponseAppendString = "";
+
+		int index = pCMD.length();
+
+		bool echo_value = false;
+
+		if (pInput[index] == '?')
+		{
+			ret_val |= CLI_SCAN_GET_OK;
+			echo_value = true;
+		}
+		else if (pInput[index] == '=')
+		{
+			int temp_value = strtol(pInput.substring(index + 1).c_str(), 0, pBase);        // decimal conversion
+
+			echo_value = true;
+
+			// check the value is within range
+			if(temp_value > pMax)
+			{
+				ret_val |= CLI_SCAN_SET_ERR_MAX;       // out of range!
+				ResponseAppendString += ",ERR_MAX";
+			}
+			else if(temp_value < pMin)
+			{
+				ret_val |= CLI_SCAN_SET_ERR_MIN;       // out of range!
+				ResponseAppendString += ",ERR_MIN";
+			}
+			else
+			{
+				if (*pValue != temp_value)
+				{
+					*pValue = temp_value;
+					ret_val |= CLI_SCAN_SET_OK;
+					ResponseAppendString += ",OK";
+				}
+				else
+				{
+					ResponseAppendString += ",SAME";
+				}
+			}
+		}
+		else
+		{
+			// invalid input - not ? or =
+			return ret_val;
+		}
+
+		if (echo_value)
+		{
+			Port->print(pOutputPrompt);
+			Port->print(pCMD);
+			Port->print("=");
+			Port->print(*pValue, pBase);
+			Port->println(ResponseAppendString);
+		}
+	}
+
 	return ret_val;
 }
 
--- src/CLI.h
+++ src/CLI.h
@@ -8,7 +8,9 @@
 #define CLI_SCAN_SET_ERR_MIN				0x8000
 #define CLI_SCAN_SET_ERR_MAX				0x4000
 	
+float CLI_Scan_float_Setting(Stream *Port, String pInput, String pCMD, float *pValue, float pMin, float pMax,const char* pFormat,String pOutputPrompt);
 int32_t CLI_Scan_INT32_Setting(Stream *Port, String pInput, String pCMD, int32_t *pValue, int32_t pMin, int32_t pMax, uint32_t pBase, String pOutputPrompt);
+int16_t CLI_Scan_INT16_Setting(Stream *Port, String pInput, String pCMD, int16_t *pValue, int16_t pMin, int16_t pMax, uint32_t pBase,String pOutputPrompt);
 uint32_t CLI_Scan_UINT32_Setting(Stream *Port, String pInput, String pCMD, uint32_t *pValue, uint32_t pMin, uint32_t pMax, uint32_t pBase, String pOutputPrompt);
 uint16_t CLI_Scan_UINT16_Setting(Stream *Port, String pInput, String pCMD, uint16_t *pValue, uint32_t pMin, uint16_t pMax, uint32_t pBase, String pOutputPrompt);
 uint8_t CLI_Scan_UINT8_Setting(Stream *Port, String pInput, String pCMD, uint8_t *pValue, uint32_t pMin, uint8_t pMax, uint32_t pBase, String pOutputPrompt);
